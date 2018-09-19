65e7af974ca4f100a462084c541ee62e7cccf078
Artem Tverdokhlebov <artem.tverdokhlebov@gmail.com>
2018-09-19 16:13:57 +0300

    allow 100% width date field

--- src/Form/Field/Date.php
+++ src/Form/Field/Date.php
@@ -39,8 +39,8 @@ class Date extends Text
 
         $this->script = "$('{$this->getElementClassSelector()}').parent().datetimepicker(".json_encode($this->options).');';
 
-        $this->prepend('<i class="fas fa-calendar fa-fw"></i>')
-            ->defaultAttribute('style', 'width: 110px');
+        $this->prepend('<i class="fas fa-calendar fa-fw"></i>');
+//            ->defaultAttribute('style', 'width: 110px');
 
         return parent::render();
     }
