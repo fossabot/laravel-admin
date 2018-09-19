54e76ec08613253d7963c53bad4d00d54fddf57e
Artem Tverdokhlebov <artem.tverdokhlebov@gmail.com>
2018-09-19 16:31:12 +0300

    fix

--- src/Traits/HasAssets.php
+++ src/Traits/HasAssets.php
@@ -45,7 +45,7 @@ trait HasAssets
         'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
 
         'vendor/laravel-admin/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js',
-        'vendor/laravel-admin/AdminLTE/dist/js/app.min.js',
+        'vendor/laravel-admin/AdminLTE/dist/js/adminlte.min.js',
         'vendor/laravel-admin/jquery-pjax/jquery.pjax.js',
         'vendor/laravel-admin/nprogress/nprogress.js',
         'vendor/laravel-admin/nestable/jquery.nestable.js',
